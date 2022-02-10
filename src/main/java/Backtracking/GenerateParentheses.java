1f0e9c6a03c95f4b6ad3970a3c97defeee54b3a0
cuining8 <cuining8@jd.com>
2022-02-10 19:51:01 +0800

    add GenerateParentheses

---
+++ src/main/java/Backtracking/GenerateParentheses.java
@@ -0,0 +1,14 @@
+package Backtracking;
+
+/**
+ * Created by cuining8 on 02/10/2020. Given n pairs of parentheses, write a function to
+ * generate all combinations of well-formed parentheses.
+ *
+ * <p>For example, given n = 3, a solution set is:
+ *
+ * <p>[ "((()))", "(()())", "(())()", "()(())", "()()()" ]
+ */
+
+public class GenerateParentheses {
+
+}
