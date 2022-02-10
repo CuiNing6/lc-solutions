f41c9d16eb285bc82c1e09d050ad5e6e8d79be0c
cuining8 <cuining8@jd.com>
2022-02-10 20:07:08 +0800

    add LetterCasePermutation

---
+++ src/main/java/Backtracking/LetterCasePermutation.java
@@ -0,0 +1,19 @@
+package Backtracking;
+
+/**
+ * Created by cuining8 on 02/10/2020. Given a string S, we can transform every letter
+ * individually to be lowercase or uppercase to create another string. Return a list of all possible
+ * strings we could create.
+ *
+ * <p>Examples: Input: S = "a1b2" Output: ["a1b2", "a1B2", "A1b2", "A1B2"]
+ *
+ * <p>Input: S = "3z4" Output: ["3z4", "3Z4"]
+ *
+ * <p>Input: S = "12345" Output: ["12345"] Note:
+ *
+ * <p>S will be a string with length at most 12. S will consist only of letters or digits.
+ *
+ * <p>Solution: O(N x 2 ^ N) Backtrack and generate all possible combinations.
+ */
+public class LetterCasePermutation {
+}
