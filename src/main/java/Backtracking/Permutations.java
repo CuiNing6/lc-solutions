0ddfab52ec58882ec57037b3cb476623aa19381c
cuining8 <cuining8@jd.com>
2022-02-15 16:38:18 +0800

    add Permutations
    Change-Id: I3e61d7cb6af6499d2448dd5f08de7795e115bd16

--- src/main/java/Backtracking/Permutations.java
+++ src/main/java/Backtracking/Permutations.java
@@ -1,5 +1,9 @@
 package Backtracking;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by cuining8 on 02/10/2022. Given a collection of distinct numbers, return all
  * possible permutations.
@@ -8,4 +12,53 @@ package Backtracking;
  * [3,1,2], [3,2,1] ]
  */
 public class Permutations {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        Permutations permutations = new Permutations();
+        List<List<Integer>> res = permutations.permutations(nums);
+        System.out.println(res);
+    }
+
+    public List<List<Integer>> permutations(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> r = new ArrayList<>();
+        boolean[] flag = new boolean[nums.length];
+        backtracking(nums, r, res, flag);
+        return res;
+    }
+
+    public void backtracking(int[] nums, List<Integer> r, List<List<Integer>> res, boolean[] flag) {
+        if (r.size() == nums.length) {
+            res.add(new ArrayList<>(r));
+        } else {
+            for (int k = 0; k < nums.length; k++) {
+                if (!flag[k]) {
+                    flag[k] = true;
+                    r.add(nums[k]);
+                    backtracking(nums, r, res, flag);
+                    r.remove(r.size()-1);
+                    flag[k] = false;
+                }
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/Backtracking/PermutationsII.java
+++ src/main/java/Backtracking/PermutationsII.java
@@ -1,5 +1,8 @@
 package Backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by cuining8 on 02/10/2022. Given a collection of numbers that might contain
  * duplicates, return all possible unique permutations.
@@ -7,4 +10,38 @@ package Backtracking;
  * <p>For example, [1,1,2] have the following unique permutations: [ [1,1,2], [1,2,1], [2,1,1] ]
  */
 public class PermutationsII {
+    public static void main(String[] args) {
+        int[] nums = {1,1,2};
+        PermutationsII permutationsII = new PermutationsII();
+        List<List<Integer>> res = permutationsII.permutationsII(nums);
+        System.out.println(res);
+    }
+
+    public List<List<Integer>> permutationsII(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> r = new ArrayList<>();
+        boolean[] flag = new boolean[nums.length];
+        backtracking(nums, r, flag, res);
+        return res;
+    }
+
+    public void backtracking(int[] nums, List<Integer> r, boolean[] flag, List<List<Integer>> res) {
+        if (r.size() == nums.length) {
+            if (!res.contains(new ArrayList<>(r))) {
+                res.add(new ArrayList<>(r));
+            }
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if(!flag[i]) {
+                    flag[i] = true;
+                    r.add(nums[i]);
+                    backtracking(nums, r, flag, res);
+                    r.remove(r.size()-1);
+                    flag[i] = false;
+                }
+            }
+        }
+    }
+
+
 }
