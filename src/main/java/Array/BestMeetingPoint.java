5d71e5408cfe9250e014bfdabc3c5587652340eb
cuining8 <cuining8@jd.com>
2022-02-17 16:58:51 +0800

    add BestMeetingPoint
    Change-Id: I9ab851d874ad02377ffb3fa5fbe6353183f3fc5e

--- src/main/java/Array/BestMeetingPoint.java
+++ src/main/java/Array/BestMeetingPoint.java
@@ -1,5 +1,9 @@
 package Array;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by cuining8 on 02/16/2022 A group of two or more people wants to meet and
  * minimize the total travel distance. You are given a 2D grid of values 0 or 1, where each 1 marks
@@ -21,4 +25,55 @@ package Array;
  * each column and then take a minimum of cartesian product of each row and each column.
  */
 public class BestMeetingPoint {
+    public static void main(String[] args) {
+        int[][] grids = {{1,0,0,0,1},{0,0,0,0,0},{0,0,1,0,0}};
+        BestMeetingPoint bestMeetingPoint = new BestMeetingPoint();
+        int res = bestMeetingPoint.bestMeetingPoint(grids);
+        System.out.println(res);
+    }
+
+
+    public int bestMeetingPoint(int[][] grids) {
+        int res = 0;
+        List<Integer> ipos = new ArrayList<Integer>();
+        List<Integer> jpos = new ArrayList<Integer>();
+
+        for(int i = 0; i < grids.length; i++){
+            for(int j = 0; j < grids[0].length; j++){
+                if (grids[i][j] == 1) {
+                    ipos.add(i);
+                    jpos.add(j);
+
+                }
+            }
+        }
+
+        for (Integer pos : ipos) {
+            res = res + Math.abs(pos - ipos.get(ipos.size()/2));
+        }
+
+        Collections.sort(jpos);
+        for (Integer pos : jpos) {
+            res = res + Math.abs(pos - jpos.get(jpos.size()/2));
+        }
+
+        return res;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
