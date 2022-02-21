e9774858ab7aa6554a6c0221797efb6c2ae27db9
CuiNing <946105802@qq.com>
2022-02-21 22:55:17 +0800

    add DeleteNode

--- src/main/java/linked_list/DeleteNode.java
+++ src/main/java/linked_list/DeleteNode.java
@@ -1,5 +1,7 @@
 package linked_list;
 
+import java.util.List;
+
 /**
  * Created by cuining8 on 02/21/2022. Write a function to delete a node (except the tail)
  * in a singly linked list, given only access to that node.
@@ -8,4 +10,42 @@ package linked_list;
  * the linked list should become 1 -> 2 -> 4 after calling your function.
  */
 public class DeleteNode {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+
+        new DeleteNode().deleteNode(listNode.next.next);
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+
+    }
+
+    public void deleteNode(ListNode node) {
+        ListNode prev = node;
+        ListNode last = node;
+        ListNode next = node.next;
+
+        while (next != null) {
+            last = prev;
+            int temp = prev.val;
+            prev.val = next.val;
+            next.val = temp;
+            prev = prev.next;
+            next = next.next;
+        }
+        last.next = null;
+    }
 }
