baa9c3bc966270f088660fb9e48e9f4255ebff26
cuining8 <cuining8@jd.com>
2022-02-23 19:55:48 +0800

    add LinkedQuickSort
    Change-Id: I1a038c3acc1c05018c2d8f3a7db30f76f821e2ee

--- src/main/java/rank/LinkedQuickSort.java
+++ src/main/java/rank/LinkedQuickSort.java
@@ -17,12 +17,54 @@ public class LinkedQuickSort {
 
 
     public static void main(String[] args) {
+        LinkedQuickSort.ListNode listNode = new LinkedQuickSort.ListNode(4);
+        listNode.next = new LinkedQuickSort.ListNode(2);
+        listNode.next.next = new LinkedQuickSort.ListNode(1);
+        listNode.next.next.next = new LinkedQuickSort.ListNode(3);
 
+        ListNode res = new LinkedQuickSort().linkedQuickSort(listNode);
+
+        while (res != null) {
+            System.out.println(res.val);
+            res = res.next;
+        }
     }
 
     public ListNode linkedQuickSort(ListNode head) {
-
+        quickSort(head, null);
         return head;
+    }
+
+    public void quickSort(ListNode head, ListNode end) {
+        if( head != end) {
+            ListNode node = sort(head, end);
+            quickSort(head, node);
+            quickSort(node.next, end);
+        }
+    }
+
+    public ListNode sort(ListNode head, ListNode end) {
+        ListNode p1 = head;
+        ListNode p2 = head.next;
+
+        while (p2 != end) {
+            if (p2.val < head.val) {
+                p1 = p1.next;
+
+                int tmp = p1.val;
+                p1.val = p2.val;
+                p2.val = tmp;
+            }
+            p2 = p2.next;
+        }
+
+        if (p1 != head) {
+            int temp = p1.val;
+            p1.val = head.val;
+            head.val = temp;
+        }
+
+        return p1;
     }
 
 
