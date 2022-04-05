c4374971b1f6c37e26ad26ece4630e76975a4078
cuining8 <cuining8@jd.com>
2022-04-05 17:33:04 +0800

    add MaximumSubarray
    Change-Id: Idd0c292dc3aa83e35e89d34f992bce665f7e3cf9

--- src/main/java/dynamic_programming/MaximumSubarray.java
+++ src/main/java/dynamic_programming/MaximumSubarray.java
@@ -1,5 +1,8 @@
 package dynamic_programming;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Created by cuining8 on 03/05/2022. Find the contiguous subarray within an array
  * (containing at least one number) which has the largest sum.
@@ -8,4 +11,24 @@ package dynamic_programming;
  * the largest sum = 6.
  */
 public class MaximumSubarray {
+    public static void main(String[] args) {
+        int[] array = {-2,1,-3,4,-1,2,1,-5,4};
+        int res = new MaximumSubarray().maximumSubarray(array);
+        System.out.println(res);
+    }
+
+    public int maximumSubarray(int[] array) {
+        int n = array.length;
+        int[] dp = new int[n];
+        dp[0] = array[0];
+
+        for (int i = 1; i < n; i++) {
+            if (array[i] > array[i] + dp[i-1]) {
+                dp[i] = array[i];
+            } else {
+                dp[i] = array[i] + dp[i-1];
+            }
+        }
+        return Arrays.stream(dp).max().getAsInt();
+    }
 }
