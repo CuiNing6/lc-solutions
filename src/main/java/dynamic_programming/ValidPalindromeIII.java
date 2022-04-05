db5862cd2b64fc6356c3507597dd2f674b164d2f
cuining8 <cuining8@jd.com>
2022-04-05 15:05:59 +0800

    add ValidPalindromeIII
    Change-Id: I4d728ae510804f02d4145b509375561e72cb02a3

--- src/main/java/dynamic_programming/ValidPalindromeIII.java
+++ src/main/java/dynamic_programming/ValidPalindromeIII.java
@@ -16,4 +16,31 @@ package dynamic_programming;
  * <p>1 <= s.length <= 1000 s has only lowercase English letters. 1 <= k <= s.length
  */
 public class ValidPalindromeIII {
+    public static void main(String[] args) {
+        String s = "abcdeca";
+        int k = 2;
+
+        int res = new ValidPalindromeIII().validPalindromeIII(s);
+
+        boolean result = s.length() - res <= k;
+
+        System.out.println(result);
+    }
+
+    public int validPalindromeIII(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = n-1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                }else {
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
 }
