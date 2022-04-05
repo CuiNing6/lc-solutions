8dd0c3b907b7e33aeae7da1da96dccc00cf7ad55
cuining8 <cuining8@jd.com>
2022-04-05 19:01:38 +0800

    add HouseRobberII
    Change-Id: Ia487568903e14f40eb66f836c8e6c726ff0ce5c0

--- src/main/java/dynamic_programming/HouseRobberII.java
+++ src/main/java/dynamic_programming/HouseRobberII.java
@@ -9,6 +9,33 @@ package dynamic_programming;
  *
  * <p>Given a list of non-negative integers representing the amount of money of each house,
  * determine the maximum amount of money you can rob tonight without alerting the police.
+ * 4,5,6,8,3 -> 13
+ * 1,5,4,2 -> 7
  */
 public class HouseRobberII {
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,8,3};
+        int res = new HouseRobberII().houseRobberII(nums);
+        System.out.println(res);
+    }
+
+    public int houseRobberII(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }else if (nums.length == 1) {
+            return nums[0];
+        }else {
+            return Math.max(rob(nums, 0, nums.length-2),rob(nums, 1, nums.length-1));
+        }
+    }
+
+    public int rob(int[] nums, int start, int end) {
+        int[] dp = new int[nums.length];
+        dp[start] = nums[start];
+        dp[start+1] = nums[start+1];
+        for (int i = start+2; i <= end; i++) {
+            dp[i] = nums[i] + dp[i-2];
+        }
+        return Math.max(dp[nums.length-1],dp[nums.length-2]);
+    }
 }
