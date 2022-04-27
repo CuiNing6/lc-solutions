56e0dcba38b90f259d767692ca3127c0997fb931
cuining8 <cuining8@jd.com>
2022-04-27 11:48:53 +0800

    add ClimbingStairs
    Change-Id: If01ccdb23ab1384b583708969af5d0b87e0df54b

--- src/main/java/dynamic_programming/ClimbingStairs.java
+++ src/main/java/dynamic_programming/ClimbingStairs.java
@@ -10,4 +10,28 @@ package dynamic_programming;
  * <p>Note: Given n will be a positive integer.
  */
 public class ClimbingStairs {
+    public static void main(String[] args) {
+        int n = 4;
+        ClimbingStairs climbingStairs = new ClimbingStairs();
+        int res = climbingStairs.climbingStairs(n);
+        System.out.println(res);
+    }
+
+    public int climbingStairs(int n) {
+        if (n==0|n==1|n==2) {
+            return n;
+        }
+
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i < n+1; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
 }
